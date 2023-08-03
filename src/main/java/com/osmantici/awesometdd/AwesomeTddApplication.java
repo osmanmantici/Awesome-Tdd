2d445a8333469165f489b71527e909dd2a894773
Osman Mantıcı <33145961+osmanmantici@users.noreply.github.com>
2023-08-04 00:17:50 +0300

    Add files via upload

---
+++ src/main/java/com/osmantici/awesometdd/AwesomeTddApplication.java
@@ -0,0 +1,13 @@
+package com.osmantici.awesometdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeTddApplication.class, args);
+	}
+
+}
